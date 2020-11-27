77650751bd6caa0c9ef7ff2e958f62b909afe3a9
Alexandre Blache <alexandreblache.pro@gmail.com>
2020-11-27 16:12:08 +0100

    contrat

--- src/main/java/fr/givemeacar/model/Client.java
+++ src/main/java/fr/givemeacar/model/Client.java
@@ -1,13 +1,15 @@
 package fr.givemeacar.model;
 
+import java.util.ArrayList;
+
 public class Client extends Account {
 
     String adresse;
+    ArrayList<Vehicule> historique = new ArrayList<Vehicule>();
 
     public Client(int id, String mail, String password, String pseudo, String name, String firstName, String adresse) {
         super();
         this.adresse = adresse;
-
 
     }
 
