74e14c10531ad75e7f5328733fbe85cedf9a3be4
romgrm <romaingreaume@gmail.com>
2020-12-01 16:51:01 +0100

    JPA base Agence + Setup Bdd Sql

--- backend/src/main/java/fr/givemeacar/model/Agence.java
+++ backend/src/main/java/fr/givemeacar/model/Agence.java
@@ -2,32 +2,38 @@ package fr.givemeacar.model;
 
 import lombok.Data;
 
-import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+//import java.util.List;
 
 @Entity // table sql
 public @Data class Agence {
 
+
     // Id auto-incrémenté
     @Id
-    @GeneratedValue(strategy = generationType.IDENTITY)        
-    int id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
 
-    String name;
-    String Localisation;
-    int nombreVehicules;
-    int vehiculeDispo;
-    int vehiculeRevision;
+    private String name;
+    private String localisation;
+    private int nombreVehicules;
+    private int vehiculeDispo;
+    private int vehiculeRevision;
 
-    List<Vehicule> stock;
-    List<Client> clientele;
+    //List<Vehicule> stock;
+    //List<Client> clientele;
 
 
-    // METHODS
+
+    /* METHODS
     public void ajouterVehicule(){
 
     }
 
     public void supprimerVehicule(){
 
-    }
+    }*/
 }
