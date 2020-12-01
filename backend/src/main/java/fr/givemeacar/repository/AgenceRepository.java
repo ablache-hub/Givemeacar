c087e9fafaf2b7a9d3a03ae73fb683ca84209a85
romgrm <romaingreaume@gmail.com>
2020-12-01 13:34:39 +0100

    Maj Pom + Jpa exemple

--- backend/src/main/java/fr/givemeacar/repository/AgenceRepository.java
+++ backend/src/main/java/fr/givemeacar/repository/AgenceRepository.java
@@ -1,3 +1,8 @@
-public interface AgenceRepository extends JpaRepository<Agence, Integer>{
+package fr.givemeacar.repository;
+
+import fr.givemeacar.model.Agence;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AgenceRepository extends JpaRepository<Agence,Integer>{
 
 }
