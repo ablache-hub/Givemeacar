cf37f52b18c018df4dca3c3ae0fa1052afa5c283
romgrm <romaingreaume@gmail.com>
2020-12-16 11:45:33 +0100

    install bootstrap

--- frontend/.history/src/App_20201216114404.js
+++ frontend/.history/src/App_20201216114404.js
@@ -1,6 +1,6 @@
 import Agence from './Components/Agence'
 import AffichageAgence from './Components/AffichageAgence'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import ''
 
 function App() {
   return (
--- frontend/.history/src/App_20201216114452.js
+++ frontend/.history/src/App_20201216114452.js
@@ -2,6 +2,7 @@ import Agence from './Components/Agence'
 import AffichageAgence from './Components/AffichageAgence'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
+
 function App() {
   return (
     <div className="App">
