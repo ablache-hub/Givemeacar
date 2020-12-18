c8e14f7cb313bb3e77f22324cfdb162dbf2d8455
Alexandre Blache <alexandreblache.pro@gmail.com>
2020-12-18 14:09:57 +0100

    profil

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,7 +12,7 @@ function App() {
             {/*<ManagerInterface/>*/}
             {/*<AddVehiculePopup/>*/}
 
-            {/*<ProfilManager/>*/}
+            <ProfilManager/>
         </div>
     );
 }
--- frontend/src/Components/ProfilManager.js
+++ frontend/src/Components/ProfilManager.js
@@ -5,15 +5,19 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 
 export default function ProfilManager() {
 
-
+    const id = 7;
     const [dataManager, setDataManager] = useState([]);
+    const [dataAgence, setDataAgence] = useState([]);
+
 
     useEffect(() => {
 
         async function fetchData() {
-            const result = await axios('http://localhost:9090/Agence/2');
-            setDataManager(result.data)
-            //console.log(result)
+
+            const url1 = await axios('http://localhost:9090/agences/' + id + '/vehicules/');
+            setDataManager(url1.data)
+            const url2 = await axios('http://localhost:9090/Agence/' + id);
+            setDataAgence(url2.data)
         }
 
         fetchData();
@@ -21,25 +25,35 @@ export default function ProfilManager() {
 
 
     return (
-        <div className="card mx-5 mt-4 col-2 col-sm-4 col-md-4 col-lg-4 col-xl-4" align="center">
-            <h5 className="card-title">{dataManager.name}</h5>
-            <img
-                src="https://product-image.juniqe-production.juniqe.com/media/catalog/product/seo-cache/x800/171/172/171-172-202L-Black/Al-Pacino-as-Tony-Montana-in-Scarface--Vintage-Photography-Archive-Affiche-sous-cadre-standard.jpg"
-                className="card-img-top rounded-circle mx-auto d-block" alt="..."
-                style={{"width": "100px", "height": "100px"}}></img>
-            <div className="card-body">
-                <h5 className="card-title">{dataManager.id}</h5>
-                <p className="card-text"></p>
-            </div>
-            <ul className="list-group list-group-flush">
-                <li className="list-group-item">{dataManager.localisation}</li>
-                <li className="list-group-item">Dapibus ac facilisis in</li>
-                <li className="list-group-item">Vestibulum at eros</li>
-            </ul>
-            <div className="card-body">
-                <a href="#" className="card-link">Card link</a>
-                <a href="#" className="card-link">Another link</a>
+        <>
+            <div className="card mx-5 mt-4 col-2 col-sm-4 col-md-4 col-lg-4 col-xl-4" align="center">
+
+                <h5 className="card-title" style={{color: '#FA983A', fontSize: '2rem'}}>{dataAgence.name}</h5>
+                <img
+                    src="https://product-image.juniqe-production.juniqe.com/media/catalog/product/seo-cache/x800/171/172/171-172-202L-Black/Al-Pacino-as-Tony-Montana-in-Scarface--Vintage-Photography-Archive-Affiche-sous-cadre-standard.jpg"
+                    className="card-img-top rounded-circle mx-auto d-block" alt="..."
+                    style={{"width": "100px", "height": "100px"}}></img>
+                <div className="card-body">
+                    <h5 className="card-title"></h5>
+                    <p className="card-text"></p>
+                </div>
+                <ul className="list-group list-group-flush">
+                    <li className="list-group-item"></li>
+                    <li className="list-group-item">
+                    </li>
+                    <li className="list-group-item">{dataManager.map((current) => (
+                        <div>
+                            <dd>{current.marque}</dd>
+                        </div>
+                    ))}
+                    </li>
+                </ul>
+                <div className="card-body">
+                    <a href="#" className="card-link">Card link</a>
+                    <a href="#" className="card-link">Another link</a>
+                </div>
+
             </div>
 
-        </div>)
+        </>)
 }
