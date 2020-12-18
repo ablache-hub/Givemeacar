55275a17606d241eb007e6550aaec9fb13b5b44e
Amin <amin.sla2015@gmail.com>
2020-12-18 13:30:34 +0100

    18/12 popup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,7 +10,7 @@ function App() {
         <div className="App">
             {/* <Agence /> */}
             {/*<ManagerInterface/>*/}
-            {/*<AddVehiculePopup/>*/}
+            <AddVehiculePopup/>
 
             {/*<ProfilManager/>*/}
         </div>
